905ac699e1b974e2daeca4b7c98506dfb230adb0
nec4eys <nec4eys@CTHULHU>
2024-07-13 15:06:36 +0700

    Добавьте файлы проекта.

---
+++ RangeTask/RangeTask.cs
@@ -0,0 +1,10 @@
+﻿namespace RangeTask
+{
+    internal class RangeTask
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
