f5ff63d6fb81738aab1d2f943552507ec3a5393c
Nec4eys <sachanechaev2000@gmail.com>
2024-07-13 18:01:05 +0700

    #

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -8,6 +8,24 @@ namespace RangeTask
 {
     internal class Range
     {
-        public double From { get; set; }
+        private double From { get; set; }
+
+        private double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number - From >= 0 && To - number >= 0;
+        }
     }
 }
