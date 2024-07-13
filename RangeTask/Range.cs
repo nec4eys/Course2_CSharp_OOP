1ad63863353f348d770620fd6fdba6e39fba4b91
Nec4eys <sachanechaev2000@gmail.com>
2024-07-13 23:05:53 +0700

    Дописан класс Range. Осталось провести тестирование

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -9,7 +9,6 @@ namespace RangeTask
     internal class Range
     {
         private double From { get; set; }
-
         private double To { get; set; }
 
         public Range(double from, double to)
@@ -26,6 +25,61 @@ namespace RangeTask
         public bool IsInside(double number)
         {
             return number - From >= 0 && To - number >= 0;
+        }
+
+        public Range GetIntersection(Range anotherRange) // поменять название функции
+        {
+            double intersectionFrom = Math.Max(From, anotherRange.From);
+            double intersectionTo = Math.Min(To, anotherRange.To);
+
+            if (intersectionTo - intersectionFrom <= 0)
+            {
+                return null;
+            }
+
+            return new Range(intersectionFrom, intersectionTo);
+        }
+
+        public Range[] GetUnification(Range anotherRange) // поменять название функции
+        {
+            Range intersectionRange = GetIntersection(anotherRange);
+
+            if (intersectionRange == null)
+            {
+                if (From != anotherRange.From && To != anotherRange.To)
+                {
+                    return [this, anotherRange];
+                }
+            }
+
+            return [new Range(Math.Min(From, anotherRange.From), Math.Max(To, anotherRange.To))];
+        }
+
+        public Range[] GetDifference(Range anotherRange) // поменять название функции
+        {
+            Range intersectionRange = GetIntersection(anotherRange);
+
+            if (intersectionRange == null)
+            {
+                return [this, anotherRange];
+            }
+
+            if ((From == anotherRange.From && To == anotherRange.To) || intersectionRange.Equals(this))
+            {
+                return [];
+            }
+
+            if (intersectionRange.Equals(anotherRange))
+            {
+                return [new Range(From, intersectionRange.From), new Range(intersectionRange.To, To)];
+            }
+
+            if (IsInside(intersectionRange.To))
+            {
+                return [new Range(intersectionRange.To, To)];
+            }
+
+            return [new Range(From, intersectionRange.From)];
         }
     }
 }
--- RangeTask/RangeTask.cs
+++ RangeTask/RangeTask.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell");
+            Range range1 = new Range(3.0, 7.0); 
+            Range range2 = new Range(3.0, 7.0); // для проверки использовать разные значения
+
+            double number = 5.0;
+
+            Console.WriteLine($"Длинна: {range1.GetLength()}");
+            Console.WriteLine($"Принадлежность числа диапазону: {range1.IsInside(number)}");
+            Console.WriteLine($"Пересечение:");
+
+            // TO DO 
+            // проверка пересечения. два варианта, для проверки возвращения null
+            // проверка объединения. два варианта, для проверки возвращения кусков
+            // проверка разности. три варианта, для проверки возвращения кусков
         }
     }
 }
