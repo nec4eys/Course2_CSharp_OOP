de1372ecf7cfe72d82b614eb8529f6d8c11bf356
Nec4eys <sachanechaev2000@gmail.com>
2024-08-30 10:12:13 +0700

    MatrixTask.

--- MatrixTask/Matrix.cs
+++ MatrixTask/Matrix.cs
@@ -349,17 +349,17 @@ public class Matrix
             throw new ArgumentOutOfRangeException(nameof(matrix1) + ", " + nameof(matrix2), $"Column count {nameof(matrix1)} != row count {nameof(matrix2)}");
         }
 
-        double[,] resultArrayMatrix = new double[matrix1.RowsCount, matrix2.ColumnsCount];
+        double[,] resultMatrixArray = new double[matrix1.RowsCount, matrix2.ColumnsCount];
 
         for (int i = 0; i < matrix1.RowsCount; i++)
         {
             for (int j = 0; j < matrix2.ColumnsCount; j++)
             {
-                resultArrayMatrix[i, j] = Vector.GetScalarProduct(matrix1._rows[i], matrix2.GetColumnByIndex(j));
+                resultMatrixArray[i, j] = Vector.GetScalarProduct(matrix1._rows[i], matrix2.GetColumnByIndex(j));
             }
         }
 
-        return new Matrix(resultArrayMatrix);
+        return new Matrix(resultMatrixArray);
     }
 
     public override string ToString()
@@ -376,7 +376,7 @@ public class Matrix
 
     public override int GetHashCode()
     {
-        int prime = 17;
+        const int prime = 17;
         int hash = 1;
 
         foreach (Vector row in _rows)
