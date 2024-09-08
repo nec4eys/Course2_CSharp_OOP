a3221c47c350c45c8bbcbd96ae63866a4324151c
Nec4eys <sachanechaev2000@gmail.com>
2024-09-08 18:51:15 +0700

    GraphTask. Создан отдельный класс для графа, добавлен рекурсивный обход в глубину, методы принимают Action, убрана внешняя логика

---
+++ GraphTask/Graph.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel;
+
+namespace GraphTask;
+
+public class Graph
+{
+    private int[,] graphMatrix;
+
+    private bool[] visitedDepthFirstSearch;
+
+    private bool[] visitedBreadthFirstSearch;
+    
+    public delegate void Function(int value);
+
+    public Graph(int[,] matrix)
+    {
+        graphMatrix = matrix;
+
+        visitedDepthFirstSearch = new bool[graphMatrix.GetLength(0)];
+        visitedBreadthFirstSearch = new bool[graphMatrix.GetLength(0)];
+    }
+
+    public void DepthFirstSearchNonRec(Function action)
+    {
+        visitedDepthFirstSearch = new bool[graphMatrix.GetLength(0)];
+
+        for (int i = 0; i < graphMatrix.GetLength(0); i++)
+        {
+            if (!visitedDepthFirstSearch[i])
+            {
+                Stack<int> stack = new Stack<int>();
+                stack.Push(i);
+                visitedDepthFirstSearch[i] = true;
+
+                while (stack.Count > 0)
+                {
+                    int node = stack.Pop();
+
+                    action(node);
+
+                    for (int j = graphMatrix.GetLength(0) - 1; j >= 0; j--)
+                    {
+                        if (graphMatrix[node, j] == 1 && !visitedDepthFirstSearch[j])
+                        {
+                            stack.Push(j);
+                            visitedDepthFirstSearch[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void DepthFirstSearchRec(Function action)
+    {
+        bool[] visited = new bool[graphMatrix.GetLength(0)];
+
+        for (int i = 0; i < graphMatrix.GetLength(0); i++)
+        {
+            if (!visited[i])
+            {
+                DepthFirstSearch(i, visited, action);
+            }
+        }
+    }
+
+    private void DepthFirstSearch(int node, bool[] visited, Function action)
+    {
+        visited[node] = true;
+
+        action(node);
+
+        for (int i = 0; i < graphMatrix.GetLength(0); i++)
+        {
+            if (graphMatrix[node, i] == 1 && !visited[i])
+            {
+                DepthFirstSearch(i, visited, action);
+            }
+        }
+    }
+
+    public void BreadthFirstSearch(Function action)
+    {
+        visitedBreadthFirstSearch = new bool[graphMatrix.GetLength(0)];
+
+        for (int i = 0; i < graphMatrix.GetLength(0); i++)
+        {
+            if (!visitedBreadthFirstSearch[i])
+            {
+                Queue<int> queue = new Queue<int>();
+                queue.Enqueue(i);
+                visitedBreadthFirstSearch[i] = true;
+
+                while (queue.Count > 0)
+                {
+                    int node = queue.Dequeue();
+
+                    action(node);
+
+                    for (int j = 0; j < graphMatrix.GetLength(0); j++)
+                    {
+                        if (graphMatrix[node, j] == 1 && !visitedBreadthFirstSearch[j])
+                        {
+                            queue.Enqueue(j);
+                            visitedBreadthFirstSearch[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- GraphTask/GraphMain.cs
+++ GraphTask/GraphMain.cs
@@ -2,84 +2,31 @@
 
 internal class GraphMain
 {
-    public static void DepthFirstSearch(int[,] graph, int start, bool[] visited)
+    public static void ActionWithNode(int node)
     {
-        Stack<int> stack = new Stack<int>();
-        stack.Push(start);
-        visited[start] = true;
-
-        while (stack.Count > 0)
-        {
-            int knot = stack.Pop();
-            Console.Write(knot + " ");
-
-            for (int i = 0; i < graph.GetLength(0); i++)
-            {
-                if (graph[knot, i] == 1 && !visited[i])
-                {
-                    stack.Push(i);
-                    visited[i] = true;
-                }
-            }
-        }
-    }
-
-    public static void BreadthFirstSearch(int[,] graph, int start, bool[] visited)
-    {
-        Queue<int> queue = new Queue<int>();
-        queue.Enqueue(start);
-        visited[start] = true;
-
-        while (queue.Count > 0)
-        {
-            int knot = queue.Dequeue();
-            Console.Write(knot + " ");
-
-            for (int i = 0; i < graph.GetLength(0); i++)
-            {
-                if (graph[knot, i] == 1 && !visited[i])
-                {
-                    queue.Enqueue(i);
-                    visited[i] = true;
-                }
-            }
-        }
+        Console.WriteLine(node + " ");
     }
 
     static void Main(string[] args)
     {
-        int[,] graph = new int[,]
+        int[,] graphMatrix =
         {
-            { 0, 1, 0, 0, 0 },
+            { 0, 1, 1, 1, 0 },
             { 1, 0, 1, 0, 0 },
-            { 0, 1, 0, 1, 0 },
-            { 0, 0, 1, 0, 1 },
-            { 0, 0, 0, 1, 0 }
+            { 1, 1, 0, 0, 1 },
+            { 1, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0 }
         };
 
+        Graph graph = new Graph(graphMatrix);
+
         Console.WriteLine("Обход в ширину:");
+        graph.BreadthFirstSearch(ActionWithNode);
 
-        bool[] visitedBFS = new bool[graph.GetLength(0)];
-        
-        for (int i = 0; i < visitedBFS.Length; i++)
-        {
-            if (!visitedBFS[i])
-            {
-                BreadthFirstSearch(graph, i, visitedBFS);
-                Console.WriteLine();
-            }
-        }
+        Console.WriteLine("Обход в глубину (не рекурсивный):");
+        graph.DepthFirstSearchNonRec(ActionWithNode);
 
-        Console.WriteLine("Обход в глубину:");
-
-        bool[] visitedDFS = new bool[graph.GetLength(0)];
-
-        for (int i = 0; i < visitedDFS.Length; i++)
-        {
-            if (!visitedDFS[i])
-            {
-                DepthFirstSearch(graph, i, visitedDFS);
-            }
-        }
+        Console.WriteLine("Обход в глубину (рекурсивный):");
+        graph.DepthFirstSearchRec(ActionWithNode);
     }
 }
