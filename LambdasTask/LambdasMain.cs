05afad0dcb5b90366e19b66bd19c1655e28e2db2
Nec4eys <sachanechaev2000@gmail.com>
2024-09-13 09:07:59 +0700

    LambdasTask. Точки перенесены в начало перенесенной строки, изменены названия

--- LambdasTask/LambdasMain.cs
+++ LambdasTask/LambdasMain.cs
@@ -4,7 +4,7 @@ internal class LambdasMain
 {
     static void Main(string[] args)
     {
-        List<Person> people = 
+        List<Person> people =
         [
             new Person("Q", 25),
             new Person("W", 17),
@@ -17,17 +17,17 @@ internal class LambdasMain
         ];
 
         // А и Б
-        List<string> uniqueNames = people.
-            Select(p => p.Name).
-            Distinct().
-            ToList();
+        List<string> uniqueNames = people
+            .Select(p => p.Name)
+            .Distinct()
+            .ToList();
 
         Console.WriteLine("Имена: " + string.Join(", ", uniqueNames) + ".");
 
         // В
-        List<Person> youngPeople = people.
-            Where(p => p.Age < 18).
-            ToList();
+        List<Person> youngPeople = people
+            .Where(p => p.Age < 18)
+            .ToList();
 
         if (youngPeople.Count != 0)
         {
@@ -39,9 +39,9 @@ internal class LambdasMain
         }
 
         // Г
-        Dictionary<string, double> averageAgesByNames = people.
-            GroupBy(p => p.Name).
-            ToDictionary(g => g.Key, g => g.Average(p => p.Age));
+        Dictionary<string, double> averageAgesByNames = people
+            .GroupBy(p => p.Name)
+            .ToDictionary(g => g.Key, g => g.Average(p => p.Age));
 
         Console.WriteLine("Средний возраст по именам:");
 
@@ -51,16 +51,16 @@ internal class LambdasMain
         }
 
         // Д
-        IEnumerable<string> peopleInDiapason = people.
-            Where(p => p.Age >= 20 && p.Age <= 45).
-            OrderByDescending(p => p.Age).
-            Select(p => p.Name);
+        IEnumerable<Person> peopleInRange = people
+            .Where(p => p.Age >= 20 && p.Age <= 45)
+            .OrderByDescending(p => p.Age)
+            .Select(p => p);
 
         Console.WriteLine("Люди, возраст которых от 20 до 45:");
 
-        foreach (string item in peopleInDiapason)
+        foreach (Person person in peopleInRange)
         {
-            Console.WriteLine(item);
+            Console.WriteLine(person.Name);
         }
     }
 }
